3a67093c9f4f5e800e196d7f93237f90b779cf16
holgw <holgworknote@gmail.com>
2025-06-08 23:50:42 +0400

    add styles

---
+++ cs16AvaloniaTheme/ViewModels/ItemView.cs
@@ -0,0 +1,13 @@
+﻿namespace cs16AvaloniaTheme.ViewModels;
+
+public class ItemView
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public ItemView(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+}
--- cs16AvaloniaTheme/ViewModels/MainViewModel.cs
+++ cs16AvaloniaTheme/ViewModels/MainViewModel.cs
@@ -3,4 +3,11 @@
 public partial class MainViewModel : ViewModelBase
 {
     public string Greeting => "Welcome to Avalonia!";
+
+    public ItemView[] Items { get; set; } =
+    [
+        new ItemView(475456, "Dog"),
+        new ItemView(2890, "Dolphin"),
+        new ItemView(267433443, "Cat"),
+    ];
 }
